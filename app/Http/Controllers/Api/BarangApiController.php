598b1d4822eafdd711da88d5f45908853a246260
Sony Darmawan <sonypiay@mail.com>
2019-01-01 23:30:31 +0700

    On branch dev

---
+++ app/Http/Controllers/Api/BarangApiController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Database\Barang;
+use App\Http\Controllers\Controller;
+
+class BarangApiController extends Controller
+{
+  public function listitems( Request $request, Barang $barang )
+  {
+    $listitem = $barang->orderBy('id_barang', 'desc')->get();
+    $data = [
+      'status' => 200,
+      'results' => $listitem
+    ];
+    return response()->json( $data, $data['status'] );
+  }
+
+  public function store( Request $request, Barang $barang )
+  {
+    $nama = $request->nama_barang;
+    $merk = $request->merk_barang;
+    $harga = $request->harga_barang;
+
+    $barang = new $barang;
+    $barang->nama_barang = $nama;
+    $barang->merk_barang = $merk;
+    $barang->harga = $harga;
+    $barang->save();
+
+    $res = [
+      'status' => 200,
+      'statusText' => 'Barang baru berhasil ditambah.'
+    ];
+
+    return response()->json( $res, $res['status'] );
+  }
+
+  public function update( Request $request, Barang $barang, $id )
+  {
+    $nama = $request->nama_barang;
+    $merk = $request->merk_barang;
+    $harga = $request->harga_barang;
+
+    $barang = $barang->where('id_barang', $id)->first();
+    $barang->nama_barang = $nama;
+    $barang->merk_barang = $merk;
+    $barang->harga = $harga;
+    $barang->save();
+
+    $res = [
+      'status' => 200,
+      'statusText' => 'Barang berhasil diupdate.'
+    ];
+
+    return response()->json( $res, $res['status'] );
+  }
+
+  public function destroy( Request $request, Barang $barang, $id )
+  {
+    $barang = $barang->where('id_barang', $id)->delete();
+    $res = [
+      'status' => 200,
+      'statusText' => 'Barang berhasil dihapus.'
+    ];
+
+    return response()->json( $res, $res['status'] );
+  }
+}
